d2ad5cec5c6b2cfd4790d5c88febdc92627c6760
Kayky Felipe do Nascimento <kayky159@gmail.com>
2024-07-31 11:42:23 -0300

    Commit 1

--- Condominio/autorizacao/models.py
+++ Condominio/autorizacao/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class CondominioDB(models.Model):
+    
+    nome_condominio = models.TextField(null=False)
+    regras_mudanca = models.TextField(null=False)
+    diretorio = models.TextField(null=False)
+    
+    class Meta:
+        db_table = 'condominioDB'
