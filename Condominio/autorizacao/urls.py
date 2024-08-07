8bd404b5188a9ca8b0c3074233e26f8368d8a1ef
jessegestin <56614539+jessegestin@users.noreply.github.com>
2024-08-06 22:24:10 -0300

    Busca de dados para condominio

--- Condominio/autorizacao/urls.py
+++ Condominio/autorizacao/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('autorizacao/', views.autorizar, name="autorizacao"),
-   #path('', views.autorizar, name='autorizacao'), 
-   path('', views.listar_condominios, name="listar_condominios"), 
+    #path('', views.autorizar, name='autorizacao'), 
+    path('', views.listar_condominios, name="listar_condominios"), 
 ]
