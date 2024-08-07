b7773b93765ea24d5ebbee3d35ba1ce09eba912f
Kayky <kayky159@gmail.com>
2024-08-06 22:07:18 -0300

    Commit

--- Condominio/autorizacao/views.py
+++ Condominio/autorizacao/views.py
@@ -5,7 +5,13 @@ from django.contrib import messages
 from django.contrib.messages import constants
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
-
+from .models import condominio_tabela
 
 def autorizar(request):
     return render(request, 'autorizacao.html')
+
+def listar_condominios(request):
+    if request.method == "GET":
+        dados = request.GET.get('condominio_tabela')
+
+    return render(request, 'listar_condominios.html', {'condominio_tabela': dados})    
